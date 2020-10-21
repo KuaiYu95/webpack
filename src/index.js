5c1255edf7c448702a195d54eef044ed501a1cf2
kuaiyu <ky.kyy@qq.com>
2020-10-21 11:03:14 +0800

    feat: webpack开发环境配置

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+import './index.less';
+import './imgs/huiyi.png';
+import './imgs/big.png';
+import './iconfont/iconfont.css';
+
+function a(x, t) {
+  return x + t;
+}
+
+console.log(a(12, 3));
